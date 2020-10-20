c8f70b47c846e9f6b19e021c049eee2a9662ae90
MP-00FMJ <73200541+MP-00FMJ@users.noreply.github.com>
2020-10-20 22:36:31 +0100

    Add files via upload

---
+++ Open-Composer.ps1
@@ -0,0 +1,17 @@
+﻿# elelvate script so network adapters can be disabled
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
+{  
+  $arguments = "& '" +$myinvocation.mycommand.definition + "'"
+  Start-Process powershell -Verb runAs -ArgumentList $arguments
+  Break
+}
+
+
+# get list of network adapters and disable them
+Get-NetAdapter | Disable-NetAdapter -Confirm:$false
+# open composer
+& "C:\Program Files (x86)\Control4\Composer\Pro\ComposerPro.exe"
+# wait
+Start-Sleep -s 5
+# re-enable network adapters
+Get-NetAdapter | Enable-NetAdapter -Confirm:$false
